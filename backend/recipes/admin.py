c3068f5ee9b7db64c764612743ffd0bba60809db
Клавдия Дунаева <klavam@yandex.ru>
2023-04-20 13:06:38 +0700

    локально работает

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -32,7 +32,9 @@ class RecipeAdmin(admin.ModelAdmin):
     @admin.display(description='Фото')
     def take_image(self, obj):
         if obj.image:
-            return mark_safe(f'<img src={obj.image.url} width="80" height="60">')
+            return mark_safe(
+                f'<img src={obj.image.url} width="80" height="60">'
+            )
         return None
 
     list_display = (
